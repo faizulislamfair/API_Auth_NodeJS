cc303f6e56fb3008cbd86ea1d4fdb22223f9476c
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2021-03-23 01:45:15 +0600

    hashing password using bcrypt

--- Models/User.model.js
+++ Models/User.model.js
@@ -1,18 +1,40 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
 
 const UserSchema = new Schema({
     email: {
         type: String,
         required: true,
         lowercase: true,
-        unique: true
+        unique: true,
     },
     password: {
         type: String,
-        required: true
+        required: true,
     }
 })
+
+UserSchema.pre('save', async function (next) {
+ try {
+     const salt = await bcrypt.genSalt(10)
+     const hashedPassword = await bcrypt.hash(this.password, salt)
+     this.password = hashedPassword
+     next()
+    } catch (error) {
+     next(error)
+ }
+
+})
+
+// UserSchema.post('save', async function (next) {
+//  try {
+//      console.log("Called after saving a user")
+//  } catch (error) {
+//      next(error)
+//  }
+
+// })
 
 const User = mongoose.model('user', UserSchema)
 module.exports = User
